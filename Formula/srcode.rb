0239cd2008caef7e664c7cbabc379627a0194c7b
Aloïs Micard <alois@micard.lu>
2021-01-18 01:13:30 +0100

    Brew formula update for srcode version v0.3.0

--- Formula/srcode.rb
+++ Formula/srcode.rb
@@ -5,17 +5,17 @@
 class Srcode < Formula
   desc "Tool that help developers to manage their codebase in an effective & productive way."
   homepage ""
-  version "0.2.0"
+  version "0.3.0"
   license "GPL-3.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/creekorful/srcode/releases/download/v0.2.0/srcode_0.2.0_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "fdc0ab2a087a92a2549f2679b39ed4123e230f26bd6cb28a3501db1480fe6aa7"
+    url "https://github.com/creekorful/srcode/releases/download/v0.3.0/srcode_0.3.0_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "bb059dda20d9b2fb571a2e054f367ddb7902ae13d68591749813bba46c7a6c70"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/creekorful/srcode/releases/download/v0.2.0/srcode_0.2.0_linux_amd64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "43dbc7eeaf53abe624a73989166a8cbcb307060d45c38d9b0aa2d30063e38d3b"
+    url "https://github.com/creekorful/srcode/releases/download/v0.3.0/srcode_0.3.0_linux_amd64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "9da3dca44c465fa8a6b087f551e91ad01f04bc07112ead93523260ec7684d0e7"
   end
 
   depends_on "git"
